17ee0875549ad176376a1b7ca718278dd515fb4b
Gerard Llorach <gerard.llorach@gmail.com>
2025-01-21 19:28:49 +0100

    fixes #4 created studentsdata and students array

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
 var a = new Student("Gerard", "Llorach", 89);
 
 var fullname = a.getFullName();
+
+
+// Define an array
+var studentsData = [
+    {
+        name: "Gerard",
+        surname: "Llorach",
+        age: 89
+    },
+    {
+        name: "Eva",
+        surname: "Garolera",
+        age: 55
+    },
+    {
+        name: "Josep",
+        surname: "Blat",
+        age: 70
+    },
+    {
+        name: "Ricardo",
+        surname: "Marqués",
+        age: 40
+    },
+    {
+        name: "Victor",
+        surname: "Ubieto",
+        age: 26
+    }
+];
+
+var students = [];
+
+for (var i = 0; i < studentsData.length; i++){
+    students.push(new Student(studentsData[i].name, studentsData[i].surname, studentsData[i].age));
+}
